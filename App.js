e2f1bae21e4bcd653ace1ece01c646eb8e8c32ab
Shivam Tripathi <shivamtripathist@gmail.com>
2022-06-17 13:42:37 +0530

    Android and ios crashes analysis on app center

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, Dimensions, Text, Button} from 'react-native';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
 const App = () => {
+  useEffect(() => {
+    checkPreviouseSession();
+  }, []);
+
+  const checkPreviouseSession = async () => {
+    const didCrash = await Crashes.hasCrashedInLastSession();
+    console.log('didCrash', didCrash);
+
+    if (didCrash) {
+      const report = await Crashes.lastSessionCrashReport();
+      alert('sory about crash, we are working on it ');
+    }
+  };
+
+  const trackEvent = () => {
+    Analytics.trackEvent('My custom event');
+  };
+
   return (
     <View style={styles.container}>
       <Text>App</Text>
-      <Button />
+      <Button
+        title="Crash"
+        onPress={() => {
+          Crashes.generateTestCrash();
+        }}
+      />
+      <Button
+        title="events"
+        onPress={() => {
+          trackEvent();
+        }}
+      />
     </View>
   );
 };
--- ios/nativeFabric/AppDelegate.mm
+++ ios/nativeFabric/AppDelegate.mm
@@ -1,4 +1,7 @@
 #import "AppDelegate.h"
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -54,6 +57,10 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   return YES;
 }
 
